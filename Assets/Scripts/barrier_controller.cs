468b08a6a2bbbf27608d071b3141fa1738c56787
ksnook24 <63332685+ksnook24@users.noreply.github.com>
2021-03-29 00:06:35 -0700

    Initial commit

---
+++ Assets/Scripts/barrier_controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrier_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int health = 0;
+
+    public Sprite[] sprites;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[health];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health > 3)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        GetComponent<SpriteRenderer>().sprite = sprites[health];
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+        health++;
+    }
+}
